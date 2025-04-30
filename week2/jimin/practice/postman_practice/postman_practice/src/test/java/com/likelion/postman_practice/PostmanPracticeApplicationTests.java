bde4766f1b4e05f5fa5b7aa21933694e57a45d31
Jimin Yun <127750424+jjmmnd@users.noreply.github.com>
2025-04-30 22:29:44 +0900

    [Feat] API 세션 실습

---
+++ week2/jimin/practice/postman_practice/postman_practice/src/test/java/com/likelion/postman_practice/PostmanPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.postman_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostmanPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ week2/jimin/practice/postman_practice/postman_practice/src/test/java/com/likelion/postman_practice/repository/ArticleRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.likelion.postman_practice.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ArticleRepositoryTest {
+
+    @Test
+    void save() {
+
+    }
+
+    @Test
+    void findById() {
+
+    }
+
+    @Test
+    void findAll() {
+
+    }
+}
