387b27f9bbb9f4107895c7060a36b5627ed8bdbd
jjmmnd <rnastery1@gmail.com>
2025-07-17 00:41:35 +0900

    [Chore] 허용할 프론트 도메인 설정

--- week6/gwanghwijimin/shopping_mall/src/main/java/likelion/shopping_mall/config/CorsConfig.java
+++ week6/gwanghwijimin/shopping_mall/src/main/java/likelion/shopping_mall/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "https://www.mustamall.store", "https://my-shop-iota-one.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
